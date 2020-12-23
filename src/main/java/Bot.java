8ebc10607f4868172a5a8e6bbffa8777aecec8fc
jackie35er <43109722+jackie35er@users.noreply.github.com>
2020-12-23 16:47:25 +0100

    Update Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -19,7 +19,7 @@ public class Bot extends ListenerAdapter
                 .addEventListeners(new Bot())
                 .setActivity(Activity.playing("Type (ping"))
                 .build();
-        System.out.println("adfs");
+        System.out.println(${{ secret.TOKEN }});
     }
 
     @Override
