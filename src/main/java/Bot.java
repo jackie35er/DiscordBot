6cf084650a4c72605162c1e8f33b67ebe12619da
BuildTools <unconfigured@null.spigotmc.org>
2020-12-23 17:35:38 +0100

    randomintGenerator Done

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,10 +1,15 @@
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.awt.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.security.auth.login.LoginException;
 
@@ -13,11 +18,11 @@ public class Bot extends ListenerAdapter
     public static void main(String[] args) throws LoginException
     {
 
-        JDABuilder.createLight("NzkxMjg0NzI3OTcyNzU3NTU0.X-M7VA.tTRNlEp2POwyA3M7f0MVd-qJTWE", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+        JDABuilder.createLight("NzkxMjg0NzI3OTcyNzU3NTU0.X-M7VA.iEMgGq5Zk1Fu5h83gPGaC98drlY", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
                 .addEventListeners(new Bot())
                 .setActivity(Activity.playing("Type (ping"))
                 .build();
-        System.out.println("adfs");
+        System.out.println("Started!");
     }
 
     @Override
@@ -30,5 +35,33 @@ public class Bot extends ListenerAdapter
             channel.sendMessage("your Mom is a hoe").queue();/* => RestAction<Message> */
         }
 
+        randomintGenerator(event);
+
     }
+
+    public void randomintGenerator(MessageReceivedEvent event)
+    {
+        Message msg = event.getMessage();
+        MessageChannel channel = event.getChannel();
+        try {
+            String[] snippets = msg.getContentRaw().split(" ");
+
+            if (snippets[0].equalsIgnoreCase("random")) {
+                int randomNum = ThreadLocalRandom.current().nextInt(Integer.parseInt(snippets[1]), Integer.parseInt(snippets[2]));
+
+                StringBuilder output = new StringBuilder("Your random Number is: ");
+                output.append(randomNum);
+                channel.sendMessage(output).queue();
+            }
+        }
+        catch(ArrayIndexOutOfBoundsException use)
+        {
+            channel.sendMessage("Wrong syntax: random min max").queue();
+            System.out.println();
+            //use.printStackTrace();
+        }
+
+    }
+
+
 }
