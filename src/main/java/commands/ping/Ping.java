fd2ed2f95bec371eda31224164921ee065b71518
jackie353er <jackie35er@gmail.com>
2020-12-24 14:21:08 +0100

    Pong updated

--- src/main/java/commands/ping/Ping.java
+++ src/main/java/commands/ping/Ping.java
@@ -1,9 +1,12 @@
 package commands.ping;
 
 import commands.Command;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.GenericMessageEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.awt.*;
 
 
 public class Ping extends Command {
@@ -16,9 +19,9 @@ public class Ping extends Command {
     public void start(MessageReceivedEvent event) {
         MessageChannel channel = event.getChannel();
         long time = System.currentTimeMillis();
-        channel.sendMessage("Pong!") /* => RestAction<Message> */
-                .queue(response /* => Message */ -> {
-                    response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
-                });
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setDescription("Pong: `"+ (System.currentTimeMillis() - time)+ "`");
+        eb.setColor(Color.blue);
+        event.getChannel().sendMessage(eb.build()).queue();
     }
 }
